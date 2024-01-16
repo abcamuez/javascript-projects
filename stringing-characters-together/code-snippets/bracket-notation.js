c4c0d6ddc91dd9ceff9009390b0b0a85b986fa35
abcamuez <82919641+abcamuez@users.noreply.github.com>
2024-01-16 00:49:00 -0500

    readings

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -1,4 +1,49 @@
-let jsCreator = "Brendan Eich";
+/*let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
+
+/*let myStr = 'Index';
+
+console.log(myStr[0]);
+console.log(myStr[4]);
+*/
+
+//let phrase = "JavaScript rocks!";
+//console.log(phrase[phrase.length - 8]);
+
+/*let word = 'Rutabaga';
+console.log(word.length);
+console.log(word[8]);
+
+//Given word = 'Rutabaga', why does word.length return the integer 8, but word[8] is undefined? There is no 8th character
+
+/*cityName = "Vienna";
+stateName = "Virginia";
+location = cityName + ", " +  stateName;
+
+console.log(location.length);
+16*/
+let pet = 'cat';
+console.log(pet + 's');
+console.log(pet += 's');
+//Because it's not changing the string but adding to it.//
+
+/*let word = 'JavaScript';
+console.log(word.toUpperCase())
+console.log(word.slice(4).toUpperCase())
+*/
+/*let language = "JavaScript";
+language.replace('J', 'Q');
+language.slice(1,6);
+console.log(language.slice(1,6));
+avaSc*/
+
+/*
+let org = "  The LaunchCode Foundation ";
+let trimmed = org.trim();
+
+console.log(trimmed);
+Answer 2 
+*/
+//Answer: console.log('Launch\nCode');
--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,11 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "Pigs";
+let name = "Karena";
+let verb = "jumps";
+let adjective = "flying";
+let color = "blue";
 
 console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
+
+let madLibs = `Javascript provides a ${color} collection of tools - including ${adjective} syntax and ${pluralNoun} - that allows ${name} to ${verb} with strings.`
+
+console.log(madLibs)
