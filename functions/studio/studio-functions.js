d99a3ca04b52d3fa52e5bab172942bebfc67aa7b
abcamuez <82919641+abcamuez@users.noreply.github.com>
2024-01-28 11:44:39 -0500

    studio

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -3,23 +3,37 @@
 // Part One: Reverse Characters
 
 // 1. Define the function as reverseCharacters. Give it one parameter, which will be the string to reverse.
-function reverseCharacters(strToReverse) {
-
-    let arr = ['hello', 'world', 123, 'orange'];
-
-arr.reverse()
-console.log(arr);
-}
-
+function reverseCharacters(strToReverse){
+  let reversed = "";
 // 2. Within the function, split the string into an array, then reverse the array.
 // 3. Use join to create the reversed string and return that string from the function.
+  return reversed = strToReverse.split('').reverse().join("")
+}
 // 4. Below the function, define and initialize a variable to hold a string.
+console.log(reverseCharacters("apple"));
 // 5. Use console.log(reverseCharacters(myVariableName)); to call the function and verify that it correctly reverses the characters in the string.
 // 6. Optional: Use method chaining to reduce the lines of code within the function.
 
 // Part Two: Reverse Digits
 
 // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
+
+function reverseCharacters (strang){
+    
+if (typeof strang === 'string'){
+
+    reversed = strang.split('').reverse().join('')
+} else 
+
+  reversed = strang.toString().split('').reverse().join('')
+
+  return reversed
+}
+
+
+console.log(reverseCharacters(8675309))
+
+
 // 2. If typeof is ‘string’, return the reversed string as before.
 // 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
 // 4. Return the reversed number.
@@ -27,7 +41,18 @@ console.log(arr);
 
 // Part Three: Complete Reversal
 
+function reversalArr(arrReverse) {
 // 1. Define and initialize an empty array.
+let arr = [];
+for (let i =0; i < arrReverse.length; i++){
+    arr.push(reverseCharacters(arrReverse[i]))
+}
+
+return arr.reverse();
+
+}
+
+
 // 2. Loop through the old array.
 // 3. For each element in the old array, call reverseCharacters to flip the characters or digits.
 // 4. Add the reversed string (or number) to the array defined in part ‘a’.
@@ -37,6 +62,10 @@ console.log(arr);
 let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
+
+console.log(reversalArr(arrayTest1));
+console.log(reversalArr(arrayTest2));
+console.log(reversalArr(arrayTest3));
 
 // Bonus Missions
 
