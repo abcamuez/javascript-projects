0a9a1fdb329d11f320b76d7bfbb77cef861cc6e8
abcamuez <82919641+abcamuez@users.noreply.github.com>
2024-03-04 21:56:16 -0500

    studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,25 @@
 //TODO: Add Your Code Below
+
+window.addEventListener("load", function() {
+    let container = this.document.getElementById("container")
+  let data = fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(res) {
+    if(res.ok) {
+        return res.json()
+    }})
+  // res.json().then(function(json) {
+    //   console.log(json)
+    // })
+    .then(function(res) {
+        res.forEach(current => {
+            container.innerHTML += `
+            <h3>${current.firstName} ${current.lastName}</h3>
+            <ul>
+            <li>Hours in Space: ${current.hoursInSpace}</li>
+            <li>Active: ${current.active}</li>
+            <h1>Skills: ${current.skills}</li>
+            <img src=${current.picture}/>`
+        });
+    })
+  })
+
+
